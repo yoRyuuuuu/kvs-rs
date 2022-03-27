e5b2ce8791e1af563f535757a310495a39eb5561
yoRyuuuuu <sasoseooo@gmail.com>
2022-03-27 23:34:30 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
-fn main() {
+pub enum Request {
+    Get { key: String },
+    Set { key: String, value: String },
+    Delete { key: String },
 }
+
+pub enum Response {
+    Get { key: String },
+    Set { key: String, value: String },
+    Delete { value: String },
+    Error { msg: String },
+}
+
+fn main() {}
